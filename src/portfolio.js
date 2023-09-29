68d2c6e9698d4e4eb3439d469b35eaeccd292797
Mohamed Osama <mosama@TradeNET.COM>
2023-09-29 21:47:39 +0300

    2.0.1

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,7 +25,7 @@ const greeting = {
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
-    "https://drive.google.com/file/d/1lRUIOXkgfHNxf1I9eMzvlvUYf2GfP96_/view?usp=sharing",
+    "https://drive.google.com/file/d/1H1UFIXniwZ4Ov-1OtrpLNM8W2q7hAViI/view?usp=sharing",
   portfolio_repository: "https://github.com/MohamedOsamaKhaled/",
   githubProfile: "https://github.com/MohamedOsamaKhaled",
 };
